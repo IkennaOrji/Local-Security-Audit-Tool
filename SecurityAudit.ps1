74bb6fb52ebb385dc329ea0adc5ad13ad34fa55c
kennathedon <iorji@villanova.edu>
2026-07-09 19:35:24 -0400

    Update SecurityAudit.ps1

--- SecurityAudit.ps1
+++ SecurityAudit.ps1
@@ -1,5 +1,5 @@
 Write-Host "====================================="
-Write-Host " Ikenna Orji's Security Audit Tool"
+Write-Host "        Security Audit Tool"
 Write-Host "====================================="
 Write-Host ""
 
